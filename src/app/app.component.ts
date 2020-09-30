34c3b6100487b610266cd5f21d0c3ecf5d03c203
berat <cedinckan@gmail.com>
2020-09-30 14:31:10 +0300

    color picker added and tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  deneme(color:string){
+    console.log(color);
+
+  }
   title = 'color-picker';
+  color='#2889e9';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ColorPickerModule} from "ngx-color-picker"
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +8,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+
+}
